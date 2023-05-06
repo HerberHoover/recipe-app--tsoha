5337c2d49119e526a879a7e26549b32b4a0e0387
Herber Hoover <elias.kousa@outlook.com>
2023-05-06 15:46:50 +0300

    new blueprints

--- recipe_app/__init__.py
+++ recipe_app/__init__.py
@@ -11,14 +11,20 @@ def create_app():
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+    
     db.init_app(app)
 
     from .main import main
     from .authentication import auth
+    from .recipes import recipes
+    from .categories import categories
+    from .ingredients import ingredients
 
     app.register_blueprint(main, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
+    app.register_blueprint(recipes, url_prefix='/')
+    app.register_blueprint(categories, url_prefix='/')
+    app.register_blueprint(ingredients, url_prefix='/')
 
     create_tables(app)
 
