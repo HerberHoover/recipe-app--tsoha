853c708ac8acdbba4a0e65151e4fd81de8026230
Herber Hoover <elias.kousa@outlook.com>
2023-05-07 19:24:32 +0300

    modified to be able to use fly.io

--- recipe_app/__init__.py
+++ recipe_app/__init__.py
@@ -9,7 +9,7 @@ load_dotenv()
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     
     db.init_app(app)
